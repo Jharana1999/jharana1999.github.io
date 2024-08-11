88efe0bfe3815fc1b89b9d9f526f97182ce22172
Ali Mohyudin <mohyudin14@gmail.com>
2024-08-12 01:07:58 +0500

    admin login updated

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -4,12 +4,14 @@
 	import '/src/styles/global.css';
 	import { user, token, isLoading } from '../stores';
 	import { goto } from '$app/navigation';
+	import AuthMaster from '../layouts/AuthMaster.svelte';
 
 	let email = '';
 	let password = '';
 	let userEmail = '';
 	let userPassword = '';
 	let error = '';
+	let errorMessage = '';
 
 	const handleUserLogin = async (e) => {
 		e.preventDefault();
@@ -77,15 +79,13 @@
 		}
 	};
 </script>
-
+<!-- 
 <main
 		style="height: 100vh; margin-top: 20vh"
 		class="container d-flex flex-column h-100 align-items-center justify-content-center"
 >
 	<h1>Welcome to Pass Management</h1>
 
-
-	<!-- New Admin Login -->
 	<h2 class="mt-5">Login</h2>
 	<form on:submit={handleAdminLogin} class="w-100" style="max-width: 500px;">
 		<div class="mb-3">
@@ -102,9 +102,72 @@
 		<button type="submit" class="btn btn-primary">Login</button>
 	</form>
 
-	<!-- Links -->
 	<p class="mt-5">Visit <a href="/login">Login</a> to get started</p>
 	<a class="btn btn-primary mb-3" href="/login-canvas">Login with Canvas</a>
 	<a class="btn btn-primary" href="/login">Login with Admin</a>
 	
 </main>
+ -->
+
+ <AuthMaster>
+	<div class="">
+		<div class="row ">
+			<div class="col-md-4 h-full bg-signin text-center text-white p-5 gap-2">
+				<h3>EGP-BROKER</h3>
+				<p></p>
+			</div>
+			<div class="col-md-8 h-full d-flex flex-column justify-content-center align-items-center">
+				<div class="text-center">
+					<h5>EGP-BROKER</h5>
+					<p class="text-muted"></p>
+				</div>
+				<form on:submit={handleAdminLogin} class="form-group">
+					<label for="email">Email:</label>
+					<input class="form-control" id="email" type="email" bind:value={userEmail} required />
+
+					<label for="password">Password:</label>
+					<input
+						class="form-control"
+						id="password"
+						type="password"
+						bind:value={userPassword}
+						required
+					/>
+
+					<button class="btn btn-primary" type="submit">Login</button>
+
+				</form>
+				<!-- <button on:click={redirectToCanvasAuth}>Login with canvas</button> -->
+
+				<p class="mt-5">Visit <a href="/login">Login</a> to get started</p>
+				<a class="btn btn-primary mb-3" href="/login-canvas">Login with Canvas</a>
+				<a class="btn btn-primary" href="/login">Login with Admin</a>
+
+
+				{#if errorMessage}
+					<p style="color: red;">{errorMessage}</p>
+				{/if}
+			</div>
+		</div>
+	</div>
+</AuthMaster>
+
+<style>
+	.bg-signin {
+		background-color: navy;
+		background-size: cover;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+	}
+
+	@media (max-width: 768px) {
+		.bg-signin{
+			display: none;
+		}
+	}
+	@media only screen and (max-width: 600px) {
+
+	}
+</style>
